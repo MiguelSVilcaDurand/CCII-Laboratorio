f03571db73b561663e536597917a7a61f67f6645
MiguelSVilcaDurand <104391603+MiguelSVilcaDurand@users.noreply.github.com>
2022-05-03 20:51:13 -0500

    Add files via upload

---
+++ laboratorio 4/ejercicio 1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+	
+void sumF(int M[3][3])
+{
+	int i,j,sum = 0;
+	cout << "\nFinding Sum of each row:\n\n";
+	for (i = 0; i < 3; ++i) {
+		for (j = 0; j < 3; ++j) {
+			sum = sum + M[i][j];
+		}
+		cout
+			<< "Suma de la fila "
+			<< i << " = " << sum
+			<< endl;
+		sum = 0;
+	}
+}
+
+int main()
+{
+	int i,j;
+	int M[3][3];
+	int x = 1;
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			M[i][j] = x++;
+		sumF(M);
+	return 0;
+}
+
+
+	
+
