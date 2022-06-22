908fc3ea7c0c4382fd4d18052e81011542fa7517
MiguelSVilcaDurand <104391603+MiguelSVilcaDurand@users.noreply.github.com>
2022-06-22 00:02:06 -0500

    Add files via upload

---
+++ laboratorio 13/ejercicio 1.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+struct student
+{
+	char nombre[20];
+	int roll;
+	int dia;
+	int mes;
+	int anio;
+} s[10];
+
+int main()
+{
+	int x;
+	cout << "Ingrese la cantidad de personas a ingresar datos: " << endl;
+	cin>>x;
+	
+	for(int i = 0; i < x; ++i)
+	{
+		s[i].roll = i+1;
+		cout << "compañero Nro: " << s[i].roll << "," << endl;
+		
+		cout << "Ingrese el nombre: ";
+		cin >> s[i].nombre;
+		
+		cout << "Fecha de Nacimiento: "<<endl;
+		cout << "Dia: ";
+		cin >> s[i].dia;
+		cout << "Mes: ";
+		cin >> s[i].mes;
+		cout << "Año: ";
+		cin >> s[i].anio;
+		
+		cout << endl;
+	}
+	
+	cout << "Informacion Recopilada: " << endl;
+	
+	for(int i = 0; i < x; ++i)
+	{
+		cout << "\nPersona: " << i+1 << endl;
+		cout << "Nombre: " << s[i].nombre << endl;
+		cout << "Fecha de nacimiento: " << s[i].dia <<"/"<< s[i].mes <<"/"<< s[i].anio<<endl;
+		if(s[i].mes == 06){
+			cout << "\nCumple años en este mes " << endl;
+		};
+	}
+	
+	return 0;
+}
---
+++ laboratorio 13/ejercicio 3.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+struct student
+{
+	char nombre[20];
+	int n;
+	int edad;
+	double talla;
+} s[10];
+
+int main()
+{
+
+	for(int i = 0; i < 10; ++i)
+	{
+		s[i].n = i+1;
+		cout << "jugador : " << s[i].n << "," << endl;
+		cout << "Nombre: ";
+		cin >> s[i].nombre;
+		cout << "Edad: ";
+		cin >> s[i].edad;
+		cout << "Talla: ";
+		cin >> s[i].talla;
+		cout << endl;
+	}
+	
+	cout << "Menores de 20 y mas altos que 1.70m: " << endl;
+
+	for(int i = 0; i < 10; ++i)
+	{
+		cout << "\nJugador: " << i+1 << endl;
+		if( s[i].edad< 20 && s[i].talla > 1.70){
+		cout << "Nombre: " << s[i].nombre << " Edad: " << s[i].edad << " Talla: " << s[i].talla << "m."<< endl;
+		} else {
+			cout << "No cumple con los requerimientos" << endl
+		}
+	}
+	
+	return 0;
+}
